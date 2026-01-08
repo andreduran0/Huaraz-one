5dbc09997075a010267e53d7cd0eddea53c991f9
andreduran0 <paqrruq@gmail.com>
2026-01-08 02:53:19 -0500

    Update geminiService.ts

--- services/geminiService.ts
+++ services/geminiService.ts
@@ -2,7 +2,7 @@
 import { GoogleGenAI } from "@google/genai";
 import { Business, Coupon } from '../types';
 
-const ai = new GoogleGenAI({ apiKey: process.env.API_KEY });
+const ai = new GoogleGenAI({ apiKey: import.meta.env.VITE_API_KEY});
 
 export const getAiResponse = async (
   prompt: string,
