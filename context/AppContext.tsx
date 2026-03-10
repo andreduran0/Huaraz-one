a06e49505cded810ce4445db19aed9df7f8cc7e8
andreduran0 <paqrruq@gmail.com>
2026-03-10 01:53:25 -0500

    Update AppContext.tsx

--- context/AppContext.tsx
+++ context/AppContext.tsx
@@ -32,7 +32,7 @@ interface AppContextType {
 }
 
 const DEFAULT_HERO_IMAGES = [
-   "https://i.imgur.com/op1mG7X.jpeg", 
+   "https://i.imgur.com/73USYqT.jpeg", 
   "https://i.imgur.com/Ub8hD9l.jpeg", 
   "https://i.imgur.com/cnfE46t.jpeg", 
   "https://bananomeridiano.com/wp-content/uploads/2022/03/laguna-paron-huaraz.jpg", 
