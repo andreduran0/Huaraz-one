fb9103286ea0a86c3b499bcb69edb1bf2563aec2
andreduran0 <paqrruq@gmail.com>
2026-07-10 02:10:26 -0500

    Update StaticMap.tsx

--- components/StaticMap.tsx
+++ components/StaticMap.tsx
@@ -47,7 +47,7 @@ export default function StaticMap({
     isEditable, 
     onBusinessMove, 
     activeCategory = 'all',
-    imageUrl = "https://i.imgur.com/lUvBsIS.jpeg" 
+    imageUrl = "https://i.imgur.com/yZ67nFL.jpeg" 
 }: InteractiveMapProps) {
     const navigate = useNavigate();
     const t = useTranslations();
