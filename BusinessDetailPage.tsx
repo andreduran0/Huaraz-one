8560ffc1c7837a870aef2d74fe026eb2b9c4ae59
andreduran0 <paqrruq@gmail.com>
2026-07-13 00:42:14 -0500

    Update BusinessDetailPage.tsx

--- BusinessDetailPage.tsx
+++ BusinessDetailPage.tsx
@@ -1,3 +1,4 @@
+import TestimonialsList from '../components/TestimonialsList';
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { useAppContext } from '../context/AppContext';
