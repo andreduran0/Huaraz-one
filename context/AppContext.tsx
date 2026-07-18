1e1785ed9d6c44cc054b4cee807ce20d5f42fe3b
andreduran0 <paqrruq@gmail.com>
2026-07-18 16:18:24 -0500

    Update AppContext.tsx

--- context/AppContext.tsx
+++ context/AppContext.tsx
@@ -32,11 +32,11 @@ interface AppContextType {
 }
 
 const DEFAULT_HERO_IMAGES = [
-   "https://i.imgur.com/j92MFOS.jpeg", 
+   "https://i.imgur.com/8geuuEn.jpeg", 
     "https://i.imgur.com/atYSlat.jpeg",
   "https://i.imgur.com/Prryi6n.jpeg", 
   "https://i.imgur.com/7JOqLFT.jpeg", 
-  "https://i.imgur.com/M1m2fRS.jpeg", 
+  "https://i.imgur.com/j92MFOS.jpeg", 
   "https://i.imgur.com/HDLq4qK.jpeg",
   "https://i.imgur.com/8Q4f1qU.jpeg"
 ];
