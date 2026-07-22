72a5cf9a5adf59e850415884679c98e817db6c8b
andreduran0 <paqrruq@gmail.com>
2026-07-22 14:06:22 -0500

    Update premium_businesses.ts

--- data/premium_businesses.ts
+++ data/premium_businesses.ts
@@ -886,7 +886,7 @@ Nos diferenciamos de las demás taquerías por ofrecer una combinación única d
 💯 Excelente relación calidad-precio.`,
     address: 'AV. Agustín Gamarra N.º 625 ref: media cuadra de la alameda Grau ',
    lat: -9.5339, 
-   lng: -77.4980,
+   lng: -77.4975,
     phone: '+51 998 127 329', 
     whatsapp: '51998127329',
     photos: [
