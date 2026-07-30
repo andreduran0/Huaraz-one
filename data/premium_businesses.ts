d506390e2794ea2b3c7d7c97f877a1c3204bc59d
andreduran0 <paqrruq@gmail.com>
2026-07-30 12:45:50 -0500

    Update premium_businesses.ts

--- data/premium_businesses.ts
+++ data/premium_businesses.ts
@@ -7,7 +7,7 @@ export const premiumBusinesses: Business[] = [
     category: BusinessCategory.RESTAURANT,
     description: 'La historia de "Mi Chef Monchi" es la historia de un sueño hecho realidad. Moisés Álvarez, conocido por todos como Monchi, inició este proyecto tras años de perfeccionar sus técnicas en restaurantes y pizzerías. Inspirado por su jefe y mentor François, de quien heredó la receta secreta de la masa perfecta, Monchi construyó con sus propias manos el horno artesanal de barro donde hoy se hornean sus famosas pizzas. No te puedes ir sin probar la Pizza de Lomo Saltado, una exquisita fusión italo-peruana, o su pizza especial con el delicioso jamón serrano regional.',
     address: 'Parque Ginebra - Huaraz, al costado de Casa de Guías',
-  lat: -9.5369, 
+  lat: -9.5389, 
     lng: -77.5097,
     phone: '+51 948262095', 
     whatsapp: '51948262095',
