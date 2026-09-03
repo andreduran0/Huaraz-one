ff0eb0a20f9624df52de6ed89000a7306f8885b3
andreduran0 <paqrruq@gmail.com>
2026-09-03 06:20:34 +0000

    Update NewsletterPage.tsx

--- pages/NewsletterPage.tsx
+++ pages/NewsletterPage.tsx
@@ -26,7 +26,7 @@ const NewsletterPage: React.FC = () => {
     setStatus('loading');
 
     try {
-      const zapierWebhookUrl = 'https://hooks.zapier.com/hooks/catch/28590861/4t5k8zn/';
+      const zapierWebhookUrl = 'https://hooks.zapier.com/hooks/catch/28736049/4hf6o2h/';
 
       const formData = new FormData();
       formData.append('whatsapp', whatsapp); 
